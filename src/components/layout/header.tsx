394b966084e4af5c62689148fc26fabc1ffd6d2b
mohamed ragav <mohamedragav56@gmail.com>
2026-01-24 16:22:58 +0000

    ## Error Type Runtime Error
    ## Error Message
    notFound() is not allowed

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -27,21 +27,20 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import LanguageSwitcher from "../LanguageSwitcher";
 import { useI18n } from "@/locales/client";
-import { useEffect, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
+import dynamic from 'next/dynamic';
+
+const LanguageSwitcher = dynamic(() => import('../LanguageSwitcher'), {
+  ssr: false,
+  loading: () => <Skeleton className="w-9 h-9 rounded-full" />,
+});
 
 export default function Header() {
   const t = useI18n();
   const { user, isUserLoading } = useUser();
   const auth = useAuth();
   const router = useRouter();
-
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   const handleSignOut = async () => {
     await signOut(auth);
@@ -81,7 +80,7 @@ export default function Header() {
           ))}
         </nav>
         <div className="flex flex-1 items-center justify-end space-x-2">
-           {mounted ? <LanguageSwitcher /> : <Skeleton className="w-9 h-9 rounded-full" />}
+           <LanguageSwitcher />
           {isUserLoading ? (
             <div className="h-8 w-24 bg-muted rounded-md animate-pulse" />
           ) : user ? (
