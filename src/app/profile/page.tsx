7b8e603dac7e22bcfd8dd049830f19e00a735de7
mohamed ragav <mohamedragav56@gmail.com>
2026-01-28 14:31:53 +0000

    لمذا هذا المشكلة وحل المشكلة عند الرفع

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ar from '@/locales/ar';
-import { useUser, useFirestore, useDoc, useMemoFirebase, useStorage } from '@/firebase';
+import { useUser, useFirestore, useDoc, useMemoFirebase, useStorage, useFirebase } from '@/firebase';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
